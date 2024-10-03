b2c7a777c33f20e4a267ad257ab23c9d22c7e38a
alyannalim <141105129+alyannalim@users.noreply.github.com>
2024-10-03 11:13:59 +0800

    Update hw4.c

--- hw4.c
+++ hw4.c
@@ -17,26 +17,29 @@ void setConsoleSize(void) {
 }
 
 void displayNvidiaSmiLayout(void) {
-    printf("+---------------------------------------------------------------------------------+\n");
-    printf("| NVIDIA-SMI 551.86                Driver Version: 551.86    CUDA Version: 12.4   |\n");
-    printf("|-------------------------------+----------------------+--------------------------|\n");
-    printf("| GPU  Name        Persistence-M | Bus-Id        Disp.A | Volatile Uncorr. ECC    |\n");
-    printf("| Fan  Temp  Perf  Pwr:Usage/Cap | Memory-Usage         | GPU-Util  Compute M.    |\n");
-    printf("|                               |                      | MIG M.                   |\n");
-    printf("|===============================+======================+==========================|\n");
-    printf("|   0  GeForce GTX 1080    On    | 00000000:26:00.0 Off | N/A                     |\n");
-    printf("| 28%%     37C    P8    11W / 180W  |  701MiB / 8192MiB    | 0%%       Default      |\n");
-    printf("+-------------------------------+----------------------+--------------------------+\n");
-
-    printf("| Processes:                                                       GPU Memory     |\n");
-    printf("|  GPU   PID  Type    Process name                                  Usage         |\n");
-    printf("|=================================================================================|\n");
-    printf("|    0  1368  C+G     C:\\Windows\\System32\\dwm.exe                     N/A         |\n");
-    printf("|    0  2116  C+G     C:\\Windows\\explorer.exe                         N/A         |\n");
-    printf("|    0  4244  C+G     C:\\Program Files\\SomeApp\\app.exe                N/A         |\n");
-    printf("|    0  5684  C+G     C:\\Games\\GameBar\\widgets.exe                    N/A         |\n");
-    printf("|    0  6672  C+G     C:\\Windows\\System32\\SearchHost.exe              N/A         |\n");
-    printf("+---------------------------------------------------------------------------------+\n");
+    // Display dummy GPU information
+    printf("+------------------------------------------------------------------------------------+\n");
+    printf("| NVIDIA-SMI 551.86               Driver Version: 551.86       CUDA Version: 12.4    |\n");
+    printf("|--------------------------------------+----------------------+----------------------+\n");
+    printf("| GPU  Name                 TCC/WDDM   | Bus-Id        Disp.A | Volatile Uncorr. ECC |\n");
+    printf("| Fan  Temp  Perf        Pwr:Usage/Cap | Memory-Usage         | GPU-Util  Compute M. |\n");
+    printf("|                                      |                      |               MIG M. |\n");
+    printf("|======================================+======================+======================|\n");
+    printf("|  0   NVIDIA GeForce GTX 1080    WDDM | 00000000:26:00.0  On |                  N/A |\n");
+    printf("| 28%%   37C    P8          11W / 180W |   701MiB /   8192MiB |       0%     Default |\n");
+    printf("|                                      |                      |                  N/A |\n");
+    printf("+--------------------------------------+----------------------+----------------------+\n\n");
+    printf("+------------------------------------------------------------------------------------+\n");
+    printf("| Processes:                                                                         |\n");
+    printf("| GPU   GI   CI     PID   Type   Process name                             GPU Memory |\n");
+    printf("|       ID   ID                                                           Usage      |\n");
+    printf("|====================================================================================|\n");
+    printf("|   0    N/A  N/A  1368  C+G    C:\\Windows\\System32\\dwm.exe              N/A      |\n");
+    printf("|   0    N/A  N/A  2116  C+G    C:\\Windows\\explorer.exe                   N/A      |\n");
+    printf("|   0    N/A  N/A  4224  C+G    C:\\Program Files\\SomeApp\\app.exe         N/A      |\n");
+    printf("|   0    N/A  N/A  56840 C+G    C:\\Games\\GameBar\\widgets.exe             N/A      |\n");
+    printf("|   0    N/A  N/A  6676  C+G    C:\\Windows\\System32\\SearchHost.exe       N/A      |\n");
+    printf("+------------------------------------------------------------------------------------+\n");
 }
 
 void main(void) {
